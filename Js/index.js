97ff86555e237a9966a48c17521ed8715a6c576b
Stevan-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-23 14:20:21 +0700

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -5,6 +5,7 @@ const chooseCross = document.getElementById("chooseCross");
 const chooseCircle = document.getElementById("chooseCircle");
 const restart = document.getElementById("restart");
 const box = document.querySelectorAll(".box");
+const overlay = document.querySelector(".overlay");
 const combinaison = [
   [0, 1, 2],
   [3, 4, 5],
@@ -15,35 +16,33 @@ const combinaison = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
-const crossData = [];
-const circleData = [];
 let board = ["", "", "", "", "", "", "", "", ""];
-let cross, circle, mutliplayers, VsComputer, i, player01, player02;
+let mutliplayers, VsComputer, player01;
+let playerSign = "X";
 
 /*************************************functions************************************/
-
 /******************Starter page**************************/
 
 // Function to launch when the starter page is open (game starter page)
 function pageLaunch() {
   starter.classList.add("activate");
   chooseCircle.classList.add("circleActive");
-  cross = false;
-  circle = true;
+  player01 = "O";
 }
 pageLaunch();
 
-//Player 1 choose the cross
-function choiceCross() {
-  chooseCross.classList.add("crossActive");
-  chooseCircle.classList.remove("circleActive");
-}
-
-//Player 1 choose the circle
-function choiceCircle() {
-  chooseCross.classList.remove("crossActive");
-  chooseCircle.classList.add("circleActive");
+//Player 1 choose the cross or the circle
+function playerChoice() {
+  if (player01 == "X") {
+    chooseCross.classList.add("crossActive");
+    chooseCircle.classList.remove("circleActive");
+    numberOfPlayer();
+  } else {
+    chooseCross.classList.remove("crossActive");
+    chooseCircle.classList.add("circleActive");
+    numberOfPlayer();
+  }
+  return;
 }
 
 //fonction to activate the Game page
@@ -52,20 +51,24 @@ function start() {
   game.classList.add("launch");
 }
 
-//fonction  to see player type of game choice
-function playerChoice() {
-  if (cross == true && VsComputer == true) {
+//fonction to see player type of game choice
+function numberOfPlayer() {
+  if ((player01 = "X" && VsComputer == true)) {
+    playerLeft.textContent = "X (You)";
+    playerRight.textContent = "0 (CPU)";
     start();
-    onePlayersP1Cross();
-  } else if (circle == true && VsComputer == true) {
+  } else if ((player01 = "O" && VsComputer == true)) {
+    playerLeft.textContent = "X (CPU)";
+    playerRight.textContent = "0 (You)";
     start();
-    onePlayersP1Circle();
-  } else if (cross == true && mutliplayers == true) {
+  } else if ((player01 = "X" && mutliplayers == true)) {
+    playerLeft.textContent = "X (P1)";
+    playerRight.textContent = "0 (P2)";
     start();
-    twoPlayersP1Cross();
-  } else if (circle == true && mutliplayers == true) {
+  } else if ((player01 = "O" && mutliplayers == true)) {
+    playerLeft.textContent = "X (P2)";
+    playerRight.textContent = "0 (P1)";
     start();
-    twoPlayersP1Circle();
   } else {
     alert = "Error";
   }
@@ -74,200 +77,114 @@ function playerChoice() {
 
 /******************game page**************************/
 
-// player against CPU player 1 choose the cross
-function onePlayersP1Cross() {
-  playerLeft.textContent = "X (You)";
-  playerRight.textContent = "0 (CPU)";
-}
-// player against CPU player 1 choose the circle
-function onePlayersP1Circle() {
-  playerLeft.textContent = "X (CPU)";
-  playerRight.textContent = "0 (You)";
-}
+// to increment the boxIndex value in the board array.
+function boxClicked() {
+  const boxIndex = this.getAttribute("boxIndex");
 
-// 2 players, player 1 choose the cross
-function twoPlayersP1Cross() {
-  playerLeft.textContent = "X (P1)";
-  playerRight.textContent = "0 (P2)";
-}
-
-// 2 players, player 1 choose the circle
-function twoPlayersP1Circle() {
-  playerLeft.textContent = "X (P2)";
-  playerRight.textContent = "0 (P1)";
-}
-
-// restart the game and back to starter page (with)
-function restartGame() {
-  window.location.reload();
-}
-
-//player 1, next player 2
-function gameDisplay(data) {
-  console.log(cross);
-  console.log(circle);
-
-  if (cross == true && data.target.classList == "box") {
-    data.target.classList.add("crossClicked");
-    winning();
-    return (cross = false), (circle = true);
-  } else if (cross == false && data.target.classList == "box") {
-    data.target.classList.add("circleClicked");
-    console.log((data.target.value = "O"));
-    winning();
-    return (cross = true), (circle = false);
-  } else if (
-    (cross == true && data.target.classList == "box circleClicked") ||
-    (cross == true && data.target.classList == "box crossClicked") ||
-    (circle == true && data.target.classList == "box circleClicked") ||
-    (circle == true && data.target.classList == "box crossClicked")
-  )
+  if (board[boxIndex] != "") {
     return;
+  }
+  updateBox(this, boxIndex);
+  checkWinner();
 }
 
-function circleWin() {
-  if ((player1 = "O")) {
-    return alert("Player 1 win");
-  } else {
-    return alert("Player 2 win ");
+//the board index will be X or O
+function updateBox(element, index) {
+  board[index] = playerSign;
+  displayImg(element);
+}
+
+//add cross and circle img at click
+function displayImg(element) {
+  if (playerSign == "X") element.classList.add("crossClicked");
+  else {
+    element.classList.add("circleClicked");
   }
 }
-function crossWin() {
-  if ((player1 = "X")) {
-    return alert("Player 1 win");
-  } else {
-    return alert("Player 2 win ");
-  }
+//change X to O after each turn
+function changePlayer() {
+  playerSign = playerSign == "X" ? "O" : "X";
 }
 
-function winning() {
-  if (
-    (box00.classList.contains("crossClicked") &&
-      box01.classList.contains("crossClicked") &&
-      box02.classList.contains("crossClicked")) ||
-    (box03.classList.contains("crossClicked") &&
-      box04.classList.contains("crossClicked") &&
-      box05.classList.contains("crossClicked")) ||
-    (box06.classList.contains("crossClicked") &&
-      box07.classList.contains("crossClicked") &&
-      box08.classList.contains("crossClicked")) ||
-    (box00.classList.contains("crossClicked") &&
-      box03.classList.contains("crossClicked") &&
-      box06.classList.contains("crossClicked")) ||
-    (box01.classList.contains("crossClicked") &&
-      box04.classList.contains("crossClicked") &&
-      box07.classList.contains("crossClicked")) ||
-    (box02.classList.contains("crossClicked") &&
-      box05.classList.contains("crossClicked") &&
-      box08.classList.contains("crossClicked")) ||
-    (box02.classList.contains("crossClicked") &&
-      box04.classList.contains("crossClicked") &&
-      box06.classList.contains("crossClicked")) ||
-    (box00.classList.contains("crossClicked") &&
-      box04.classList.contains("crossClicked") &&
-      box08.classList.contains("crossClicked"))
-  )
-    return crossWin();
-  else if (
-    (box00.classList.contains("circleClicked") &&
-      box01.classList.contains("circleClicked") &&
-      box02.classList.contains("circleClicked")) ||
-    (box03.classList.contains("circleClicked") &&
-      box04.classList.contains("circleClicked") &&
-      box05.classList.contains("circleClicked")) ||
-    (box06.classList.contains("circleClicked") &&
-      box07.classList.contains("circleClicked") &&
-      box08.classList.contains("circleClicked")) ||
-    (box00.classList.contains("circleClicked") &&
-      box03.classList.contains("circleClicked") &&
-      box06.classList.contains("circleClicked")) ||
-    (box01.classList.contains("circleClicked") &&
-      box04.classList.contains("circleClicked") &&
-      box07.classList.contains("circleClicked")) ||
-    (box02.classList.contains("circleClicked") &&
-      box05.classList.contains("circleClicked") &&
-      box08.classList.contains("circleClicked")) ||
-    (box00.classList.contains("circleClicked") &&
-      box04.classList.contains("circleClicked") &&
-      box08.classList.contains("circleClicked")) ||
-    (box02.classList.contains("circleClicked") &&
-      box04.classList.contains("circleClicked") &&
-      box06.classList.contains("circleClicked"))
-  )
-    return circleWin();
-}
-
-/*
+//check if winning combinaison match
 function checkWinner() {
-  let roundWon = false;
-  console.log(roundWon);
-
+  let won = false;
   for (let i = 0; i < combinaison.length; i++) {
     const condition = combinaison[i];
-    console.log(condition);
-    const boxA = board[condition[0]];
-    const boxB = board[condition[1]];
-    const boxC = board[condition[2]];
-    console.log(boxA);
-    console.log(boxB);
-    console.log(boxC);
+    let a = board[condition[0]];
+    let b = board[condition[1]];
+    let c = board[condition[2]];
 
-    if (boxA == "" || boxB == "" || boxC == "") {
+    if (a == "" || b == "" || c == "") {
       continue;
     }
-    if (boxA == boxB && boxB == boxC) {
-      console.log(roundWon);
-      roundWon = true;
+    if (a == b && b == c) {
+      won = true;
       break;
     }
-    console.log(roundWon);
+  }
+  if (won) {
+    displayWinner();
+  } else if (!board.includes("")) {
+    displayDraw();
+  } else {
+    changePlayer();
   }
 }
 
-checkWinner();
-*/
+//display the winner popup
+function displayWinner() {
+  if (player01 == "O" && playerSign == "O") {
+    overlay.classList.add("activating");
+  } else if (player01 == "O" && playerSign == "X") {
+    overlay.classList.add("activating");
+  } else if (player01 == "X" && playerSign == "X") {
+    overlay.classList.add("activating");
+  } else {
+    overlay.classList.add("activating");
+  }
+}
+
+//display the drow popup
+function displayDraw() {
+  overlay.classList.add("activating");
+}
 /*************************************AddEventListener************************************/
 
 //for each button
 box.forEach((element) => {
-  element.addEventListener("click", (data) => {
-    gameDisplay(data);
-  });
+  element.addEventListener("click", boxClicked);
 });
 
 //choose the cross (starter page)
 chooseCross.addEventListener("click", (e) => {
-  cross = true;
-  circle = false;
   player01 = "X";
-  choiceCross();
+  playerChoice();
 });
 
 //choose the circle (starter page)
 chooseCircle.addEventListener("click", (e) => {
-  cross = false;
-  circle = true;
   player01 = "O";
-  choiceCircle();
+  playerChoice();
 });
 
 //play against CPU (starter page)
-vsCpu.addEventListener("click", (e) => {
+vsCpu.addEventListener("click", () => {
   VsComputer = true;
   mutliplayers = false;
-  playerChoice();
+  numberOfPlayer();
 });
 
 //play against an other player (starter page)
-players.addEventListener("click", (e) => {
+players.addEventListener("click", () => {
   VsComputer = false;
   mutliplayers = true;
-  playerChoice();
+  numberOfPlayer();
 });
 
 //restart button (game page)
 restart.addEventListener("click", () => {
-  restartGame();
+  window.location.reload();
 });
 
 /*
